2a87f88fcd40433598b6949dbfbceb68670ab4ba
Kushal <kushal@kushal-Vostro-3550.(none)>
2013-08-28 07:50:18 -0400

    modified to urldecode and download ADS ppt

--- scrape.py
+++ scrape.py
@@ -1,17 +1,19 @@
-import urllib2
+from urllib2 import urlopen
+from urllib import unquote_plus
 import shutil
 from BeautifulSoup import BeautifulSoup
 import re
 
-DIR_PATH = "/extra/Fall2013/MIS/"
-URL_PATH = "http://ia700306.us.archive.org/22/items/MIT18.06S05_MP4/"
-EXT = "mp4"
+DIR_PATH = "/extra/Fall2013/ADS/PPT/"
+URL_PATH="http://www.cise.ufl.edu/~sahni/cop5536/powerpoint/"
+#URL_PATH = "http://ia700306.us.archive.org/22/items/MIT18.06S05_MP4/"
+EXT = "ppt"
 
 pattern = re.compile("/?([^\.]+)." + EXT)
-for link in BeautifulSoup(urllib2.urlopen(URL_PATH).read()).findAll("a"):
+for link in BeautifulSoup(urlopen(URL_PATH).read()).findAll("a"):
     match = pattern.search(link["href"])
     if match:
-        response = urllib2.urlopen(URL_PATH + link["href"])
-        out_file = open(DIR_PATH + match.group(1) + "." + EXT, 'wb')
+        response = urlopen(URL_PATH + link["href"])
+        out_file = open(DIR_PATH + unquote_plus(match.group(1)) + "." + EXT, 'wb')
         shutil.copyfileobj(response, out_file)
 
